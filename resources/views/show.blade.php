0a20e4fe9dc8f2d2a8a51b64d71a5e6ad080aff2
PaingHeinKyaw <paingheink003@gmail.com>
2026-05-21 12:20:19 +0630

    completed displaying books with dynamic data from database

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -17,10 +17,6 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Books Table</h3>
-                    </div>
-
                     <div class="card-body">
                         <table
                             id="books"
@@ -37,24 +33,21 @@
                             </thead>
 
                             <tbody>
+                                @foreach($books as $book)
                                 <tr>
-                                    <td>Smth</td>
-                                    <td>Smth</td>
-                                    <td>Smth</td>
-                                    <td>Smth</td>
-                                    <td>Action</td>
+                                    <td>{{ $book->title }}</td>
+                                    <td>{{ $book->author }}</td>
+                                    <td>{{ $book->description }}</td>
+                                    <td>{{ $book->published_date }}</td>
+                                    <td>
+                                        <div class="d-flex justify-content-between">
+                                            <a class="btn btn-default mr-2" href="">Edit</a>
+                                            <a class="btn btn-default" href="">Delete</a>
+                                        </div>
+                                    </td>
                                 </tr>
+                                @endforeach
                             </tbody>
-
-                            <tfoot>
-                                <tr>
-                                    <th>Title</th>
-                                    <th>Author</th>
-                                    <th>Description</th>
-                                    <th>Published Date</th>
-                                    <th>Action</th>
-                                </tr>
-                            </tfoot>
                         </table>
                     </div>
                 </div>
