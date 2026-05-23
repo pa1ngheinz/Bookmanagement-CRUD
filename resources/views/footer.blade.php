2342cf027a42117a4a7563c672796620d81e0e67
PaingHeinKyaw <paingheink003@gmail.com>
2026-05-23 11:37:53 +0630

    modified footer blade

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 </strong>
+    <strong>Copyright &copy; 2026 </strong>
 
     <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">Anything you want</div>
+    <div class="float-right d-none d-sm-inline">Paing Hein Kyaw</div>
 </footer>
